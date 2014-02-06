0635882e48783e685b4010b544ac4e8ac7589c53
Keval Langalia <keval.langalia@live.com>
2014-02-06 20:24:19 +0530

    Created project on git

---
+++ 8 Zip/8 Zip/Helper/FileFolderModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Zip.Helper
+{
+    public class FileFolderModel
+    {
+        public string name { get; set; }
+        public bool isFolder { get; set; }
+    }
+}
---
+++ 8 Zip/8 Zip/UserControls/BusyBar.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _8_Zip.UserControls
+{
+    public sealed partial class BusyBar : UserControl
+    {
+        public BusyBar()
+        {
+            this.InitializeComponent();
+            this.IsBusy = false;
+        }
+
+        public bool IsBusy
+        {
+            get { return (bool)GetValue(IsBusyProperty); }
+            set
+            {
+                SetValue(IsBusyProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for IsBusy.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsBusyProperty =
+            DependencyProperty.Register("IsBusy", typeof(bool), typeof(BusyBar), new PropertyMetadata(0, OnPropertyChanged));
+
+        private static void OnPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            try
+            {
+                BusyBar control = source as BusyBar;
+                if ((bool)e.NewValue)
+                {
+                    control.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    control.Visibility = Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
